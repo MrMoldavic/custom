af57316e4f2478cdb9a078b9a6ac37805d7e0a91
MrMoldavic <baptiste.diodati@gmail.com>
2023-09-21 11:33:44 +0200

    .

--- admin/adminindex.php
+++ admin/adminindex.php
@@ -111,7 +111,7 @@ $formfile = new FormFile($db);
 $souhait = new Souhait($db);
 
 llxHeader("", $langs->trans("Admin"));
-print var_dump("Version Git : 1.0.0");
+print "<h1>Version Git : 1.0.0</h1>";
 print load_fiche_titre($langs->trans("Zone Admin"), '', $user->picto);
 
 $formconfirm = '';
